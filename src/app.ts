d240c801a5667a1ee8c0f539dba0e64519283537
aloksingh3112 <aloksingh3112@gmail.com>
2019-01-10 13:45:05 +0530

    added void type

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
-const firstNum:number =10;
-const secondNum:number=20;
+// let firstNum:number =10;
+// let secondNum=20;
 
 
-function calcSum(a:number,b:number):number{
-   return a+b
+// function calcSum(a:number,b:number):number{
+//    return a+b
+// }
+
+// const sum=calcSum(firstNum,secondNum);
+
+// console.log(`sum is ${sum}`);
+
+
+
+/*  *** Void Type    ***  */
+
+let firstname:string="alok";
+
+function setName(name:string):void{
+   firstname=name
 }
+console.log(firstname);
+setName("avinash");
+console.log(firstname);
 
-const sum=calcSum(firstNum,secondNum);
-
-console.log(`sum is ${sum}`);
